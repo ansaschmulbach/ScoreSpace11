9d10d30dfce5d7a1d6c466b4a9fe2dce248a1318
Oliver Puffer <pufferoliver@berkeley.edu>
2021-01-10 10:02:05 -0800

    Wrote scripts for entering name

--- ScoreSpace11/Assets/Scripts/GameManager.cs
+++ ScoreSpace11/Assets/Scripts/GameManager.cs
@@ -44,6 +44,7 @@ public class GameManager : MonoBehaviour
     public void LoadToLeaderboard()
     {
         SceneManager.LoadScene("leaderboard");
+        GameManager.instance.LoadToLeaderboard();
     }
 
 }
---
+++ ScoreSpace11/Assets/Scripts/ScoreBoard/GetName.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GetName : MonoBehaviour
+{
+    public ScoreBoard board;
+    public TMP_InputField iField;
+    private string myName;
+    
+    public void storeString()
+    {
+        myName = iField.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.storeString();
+    }
+
+    public void uploadScores()
+    {
+        board.AddNewHighscore(this.myName, GameManager.instance.gameState.score);
+    }
+}
