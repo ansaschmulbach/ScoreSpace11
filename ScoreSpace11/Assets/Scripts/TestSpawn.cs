5411ce30873101b701b7af6571dd1d4f6324b2be
Oliver Puffer <pufferoliver@berkeley.edu>
2021-01-09 19:01:49 -0800

    Created script to spawn cows

---
+++ ScoreSpace11/Assets/Scripts/TestSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpawnDestroy spwn;
+    void Start()
+    {
+        spwn.SpawnAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
